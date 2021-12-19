24297c06037e4d90944cbdf52224824c9015fb2b
Asandei Stefan <stefan.asandei@protonmail.com>
2021-12-19 13:06:31 +0200

    I hope this is a pwa, now

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,33 @@
 import { VitePWA } from "vite-plugin-pwa";
 
 export default {
-  plugins: [VitePWA()],
+  plugins: [
+    VitePWA({
+      includeAssets: ["favicon.ico", "robots.txt"],
+      manifest: {
+        name: "Asandei Stefan's Portfolio",
+        short_name: "Asandei Stefan",
+        description: "The Portfolio of Asandei Stefan.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "192x192",
+            type: "image/x-icon",
+          },
+          {
+            src: "favicon.ico",
+            sizes: "512x512",
+            type: "image/x-icon",
+          },
+          {
+            src: "favicon.ico",
+            sizes: "512x512",
+            type: "image/x-icon",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 };
